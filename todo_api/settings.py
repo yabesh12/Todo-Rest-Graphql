882e5d05f62156bd4cf4fd8d41381194440f3786
Yabesh <yabesh@deepsense.in>
2023-02-13 17:30:11 +0530

    static added

--- todo_api/settings.py
+++ todo_api/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
+        os.path.join(BASE_DIR, 'staticfiles')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
