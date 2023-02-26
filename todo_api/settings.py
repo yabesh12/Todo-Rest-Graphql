b8f581148908b94abb51be5c03b62620f9559dc5
Yabesh <yabesh@deepsense.in>
2023-02-26 10:25:29 +0530

    aws s3 settings added

--- todo_api/settings.py
+++ todo_api/settings.py
@@ -207,16 +207,16 @@ if do_space is True:
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
     }
-AWS_ACCESS_KEY_ID = "AKIA5BNZZ4736SQUK4XF"
-AWS_SECRET_ACCESS_KEY = "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"
-AWS_STORAGE_BUCKET_NAME = 'django-aws-bucket-1995'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_DEFAULT_ACL = None
-print(AWS_S3_CUSTOM_DOMAIN)
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_LOCATION = "todo-api/static"
+# AWS_ACCESS_KEY_ID = "AKIA5BNZZ4736SQUK4XF"
+# AWS_SECRET_ACCESS_KEY = "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"
+# AWS_STORAGE_BUCKET_NAME = 'django-aws-bucket-1995'
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# AWS_DEFAULT_ACL = None
+# print(AWS_S3_CUSTOM_DOMAIN)
+# AWS_S3_OBJECT_PARAMETERS = {
+#     'CacheControl': 'max-age=86400',
+# }
+# AWS_LOCATION = "todo-api/static"
 
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
