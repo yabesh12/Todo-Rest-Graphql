3486432b90fae81b099c9a9c9cadc2c558ae0477
Yabesh <yabesh@deepsense.in>
2023-03-04 17:49:41 +0530

    s3

--- todo_api/settings.py
+++ todo_api/settings.py
@@ -227,7 +227,7 @@ if do_space is True:
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    # AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
     AWS_S3_ENDPOINT_URL=os.environ.get("AWS_S3_ENDPOINT_URL")
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
